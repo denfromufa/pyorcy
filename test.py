6a85f9e3e445e9fb362122e8789d8435c33ac60a
markolopa <FFA195@WIN.CORP.com>
2016-03-04 09:58:33 +0100

    comments and test output improved for more clarity

--- test.py
+++ test.py
@@ -7,14 +7,18 @@ from compute import f
 
 def main():
     n = int(sys.argv[1])
-    timef(n, False)
-    timef(n, True)
+    t1 = timef(n, False)
+    t2 = timef(n, True)
+    if t2 != 0:
+        print("speedup: %.1f" % (t1 / t2))
     
 def timef(n, use_cython):
     pyorcy.USE_CYTHON = use_cython
     t1 = time()
     v = f(n, n)
+    delta = time() - t1
     print("n = %d f = %.1f use_cython = %s time: %.3fs"
-          % (n, v, use_cython, time() - t1))
+          % (n, v, use_cython, delta))
+    return delta
 
 main()
